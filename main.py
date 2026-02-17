0d207dfb8b6936c89d0d1a204e1034da6d93387b
paragghosh99 <ghosh.parag2025@gmail.com>
2026-02-17 20:14:08 +0530

    Added frontend to the existing api app

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from fastapi import FastAPI
-from schemas import IngestRequest, QuestionRequest
+from fastapi import FastAPI, UploadFile, File
+from schemas import QuestionRequest
 from ingestion import ingest_pdf
 from rag import answer_question
+import shutil
+import os
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+UPLOAD_DIR = "uploaded_pdfs"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 
 @app.post("/ingest")
-def ingest(req: IngestRequest):
-    chunks = ingest_pdf(req.pdf_path)
+async def ingest(file: UploadFile = File(...)):
+    if not file.filename:
+        return {"error": "Invalid file"}
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    chunks = ingest_pdf(file_path)
+
     return {"status": "ingested", "chunks": chunks}
 
 
